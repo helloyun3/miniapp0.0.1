d9811310cbb11f5834eb7b8f81a7f805bb49a556
student <student@hubu.edu.cn>
2019-04-03 11:44:05 +0800

    sd

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,82 @@
 <template>
   <div @click="clickHandle">
-     <i-notice-bar icon="systemprompt" loop>
-    2019年4月1日举行吃货节
+    <i-notice-bar v-if="judegment === ture" icon="systemprompt" loop>
+    {{notice}}举行吃货节
     </i-notice-bar>
+    <i-grid i-class="no-border">
+    <i-grid-item v-for="item in grids" :key="item" i-class="no-border">
+      <i-grid-label>{{item}}</i-grid-label>
+    </i-grid-item >
+    </i-grid>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/1.png" />
+        </i-grid-icon>
+        <i-grid-label>西点</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/2.png" />
+        </i-grid-icon>
+        <i-grid-label>主食</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/3.png" />
+        </i-grid-icon>
+        <i-grid-label>外卖</i-grid-label>
+    </i-grid-item>
+</i-grid>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/4.png" />
+        </i-grid-icon>
+        <i-grid-label>饮品</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/5.png" />
+        </i-grid-icon>
+        <i-grid-label>零食</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/6.png" />
+        </i-grid-icon>
+        <i-grid-label>面食</i-grid-label>
+    </i-grid-item>
+</i-grid>
+    <i-panel title="吃货推荐">
+      <view class="top-padding">
+      <i-card title="良品铺子" extra="零食" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">好多不错的零食</view>
+        <view slot="footer">后街1-22</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      <view class="top-padding"></view>
+    </view>
+    </i-panel>
   </div>
 </template>
 
@@ -12,6 +86,9 @@ import card from '@/components/card'
 export default {
   data () {
     return {
+      judegment:false,
+      notice: '2019年4月3日',
+      grids: ['西点','主食','外卖','饮品','零食','面食'],
       motto: 'Hello miniprograme',
       userInfo: {
         nickName: 'mpvue',
@@ -46,6 +123,12 @@ export default {
 </script>
 
 <style scoped>
+div >>> .no-border {
+  border-width: 0pt;
+}
+.top-padding {
+  padding-top: 50rpx;
+}
 .userinfo {
   display: flex;
   flex-direction: column;
